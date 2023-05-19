b1e37780e2d2bf49060016955b04775c40261743
Cesarsolis0 <Cesarsolis0@users.noreply.github.com>
2023-05-19 12:14:46 -0400

    laboratorio 2 agregado

---
+++ clase 06.dart
@@ -0,0 +1,15 @@
+// 18 mayo guia numero 1
+// 19 mayo laboratorio 2 ; 1 ejercicio lista en dart y 1 de matrices en python
+// 26 mayo parcial ; 3 algoritmos
+
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Introduce una lista de números enteros separados por espacios: ");
+//   String numerosStr = stdin.readLineSync()!;
+//   List<int> numeros = numerosStr.split(" ").map((str) => int.parse(str)).toList();
+//   int suma = numeros.reduce((a, b) => a + b);
+//   print("La suma de los números es: $suma");
+// }
+
+   
